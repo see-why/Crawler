296c32d20ec65fabba31bb7aa9dde48a3dbc0556
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-08-25 23:18:34 -0400

    Add normalizeURL function skeleton

---
+++ normalize_url.go
@@ -0,0 +1,7 @@
+package main
+
+// normalizeURL takes a URL string and returns its normalized form.
+func normalizeURL(rawURL string) (string, error) {
+	// TODO: implement normalization logic
+	return "", nil
+}
