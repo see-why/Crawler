f797c16fecf3068e5867cee0126f10c45327a9e4
Osita Cyril Iyadi <Iyadicy@gmail.com>
2025-08-25 23:19:45 -0400

    Implement URL normalization logic in normalizeURL function

--- normalize_url.go
+++ normalize_url.go
@@ -1,7 +1,27 @@
+
 package main
+
+import (
+	"net/url"
+	"strings"
+)
 
 // normalizeURL takes a URL string and returns its normalized form.
 func normalizeURL(rawURL string) (string, error) {
-	// TODO: implement normalization logic
-	return "", nil
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return "", err
+	}
+
+	host := strings.TrimPrefix(u.Hostname(), "www.")
+
+	path := strings.TrimSuffix(u.EscapedPath(), "/")
+
+	// Rebuild normalized URL: host + path
+	normalized := host
+	if path != "" {
+		normalized += path
+	}
+
+	return normalized, nil
 }
